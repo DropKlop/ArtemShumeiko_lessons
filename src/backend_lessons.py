da029789117549c69faa0013901006f43e6f2c73
Ruslan <rootpravil@gmail.com>
2025-08-30 15:37:24 +0300

    пагинация

--- src/backend_lessons.py
+++ src/backend_lessons.py
@@ -1,14 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.api.hotels import router as router_hotel
-
 import sys
 from pathlib import Path
 
-
-
 sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotel
+from src.config import settings
+
+print(f"{settings.DB_NAME=}")
 
 app = FastAPI()
 app.include_router(router_hotel)
