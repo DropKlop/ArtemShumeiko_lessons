1be49365267c66c65c9961adf0d157213506e299
Ruslan <rootpravil@gmail.com>
2025-09-04 12:38:24 +0300

    Сделал поле email уникальным

--- src/api/dependecies.py
+++ src/api/dependecies.py
@@ -8,4 +8,5 @@ class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(1, ge=1)]
     per_page: Annotated[int | None, Query(3, gt=1, le=3)]
 
+
 PaginationDep = Annotated[PaginationParams, Depends()]
--- src/backend_lessons.py
+++ src/backend_lessons.py
@@ -6,9 +6,13 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+
 from src.api.hotels import router as router_hotel
+from src.api.auth import router as router_auth
 
 app = FastAPI()
+
+app.include_router(router_auth)
 app.include_router(router_hotel)
 
 if __name__ == "__main__":
