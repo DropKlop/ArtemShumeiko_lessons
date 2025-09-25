7545b6cb6488d0d722a88a0384a9cd7c18c53a30
Ruslan <rootpravil@gmail.com>
2025-09-25 15:38:11 +0300

    тестирование bookings

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,10 @@ from src.schemas.hotels import HotelAdd
 from src.schemas.rooms import RoomAdd
 from src.utils.db_manager import DBManager
 
+@pytest.fixture()
+async def db():
+    async with DBManager(session_factory=async_sessionmaker_maker_null_pull) as db:
+        yield db
 
 @pytest.fixture(scope="session", autouse=True)
 def check_mode():
@@ -23,28 +27,29 @@ async def setup_db(check_mode):
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    #после создания добавим сразу тестовые данные
+
     hotels = json_to_model("tests/mock_hotels.json", HotelAdd)
     rooms = json_to_model("tests/mock_rooms.json", RoomAdd)
 
-    async with DBManager(session_factory=async_sessionmaker_maker_null_pull) as db:
-        await db.hotels.add_bulk(hotels)
-        await db.rooms.add_bulk(rooms)
-        await db.commit()
+    async with DBManager(session_factory=async_sessionmaker_maker_null_pull) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
 
 def json_to_model(file_src:str, model):
     with open(file_src, "r", encoding="utf-8") as file:
         data = json.load(file)
         return [model.model_validate(item) for item in data]
 
-
+@pytest.fixture(scope="session")
+async def ac():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        yield ac
 
 @pytest.fixture(scope="session", autouse=True)
-async def test_root(setup_db):
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test") as ac:
-         await ac.post("/auth/register",
-                                 json={
-                                     "email":"pupis@pes.com",
-                                     "password": "pupis123"
-                                 })
+async def test_root(ac, setup_db):
+    await ac.post("/auth/register",
+        json={
+        "email":"pupis@pes.com",
+        "password": "pupis123"
+        })
--- tests/intergation_tests/hotels/test_db.py
+++ tests/intergation_tests/hotels/test_db.py
@@ -1,11 +1,7 @@
-from src.database import async_sessionmaker_maker_null_pull
 from src.schemas.hotels import HotelAdd
-from src.utils.db_manager import DBManager
 
 
-async def test_create_hotel():
+async def test_create_hotel(db):
     hotel_data = HotelAdd(title= "Hotel 5 star", location="Краснодар")
-    async with DBManager(session_factory=async_sessionmaker_maker_null_pull) as db:
-        new_data = await db.hotels.add_(hotel_data)
-        await db.commit()
-        print(f"{new_data=}")
+    await db.hotels.add_(hotel_data)
+    await db.commit()
