699f727329d0c20b5136fff077c44e64ac8eacd3
Ruslan <rootpravil@gmail.com>
2025-09-25 14:05:06 +0300

    pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,32 @@
+import json
+
 import pytest
 
+from src.backend_lessons import app
 from src.config import settings
 from src.database import Base, engine_null_pull
 from src.models import *
 
-@pytest.fixture(scope="session", autouse=True)
-async def async_main():
-    assert settings.MODE == "TEST" #Проверка на тестовую среду, чтобы случайно не удалить таблицы в рабочей базе
+from httpx import ASGITransport, AsyncClient
 
+@pytest.fixture(scope="session", autouse=True)
+def check_mode():
+    assert settings.MODE == "TEST"
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_db(check_mode):
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def test_root(setup_db):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test") as ac:
+         await ac.post("/auth/register",
+                                 json={
+                                     "email":"pupis@pes.com",
+                                     "password": "pupis123"
+                                 })
--- tests/intergation_tests/hotels/test_db.py
+++ tests/intergation_tests/hotels/test_db.py
@@ -1,11 +1,11 @@
-from src.database import async_sessionmaker_maker
+from src.database import async_sessionmaker_maker_null_pull
 from src.schemas.hotels import HotelAdd
 from src.utils.db_manager import DBManager
 
 
 async def test_create_hotel():
     hotel_data = HotelAdd(title= "Hotel 5 star", location="Краснодар")
-    async with DBManager(session_factory=async_sessionmaker_maker) as db:
+    async with DBManager(session_factory=async_sessionmaker_maker_null_pull) as db:
         new_data = await db.hotels.add_(hotel_data)
         await db.commit()
         print(f"{new_data=}")
