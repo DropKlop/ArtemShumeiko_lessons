46f796fd27ce54c5c8b49eeb60a5071a5f750bfd
Ruslan <rootpravil@gmail.com>
2025-09-26 13:20:38 +0300

    тестирование api facilities

---
+++ tests/intergation_tests/facilities/test_api.py
@@ -0,0 +1,17 @@
+async def test_get_facilities(ac):
+    facilities = await ac.get(
+        "/facilities"
+    )
+    print(f"{facilities=}")
+
+    assert facilities.status_code == 200
+
+async def test_add_facilities(ac):
+    facilities = await ac.post(
+        "/facilities",
+        json={
+            "title": "Тестовые удобства"
+        }
+    )
+
+    assert facilities.status_code == 200
